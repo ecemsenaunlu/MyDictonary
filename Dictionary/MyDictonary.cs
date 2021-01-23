32acecf38a5d7e04a619ea5d8abc9e1d74240df2
ecem <ecemsenaunlu@gmail.com>
2021-01-23 17:51:37 +0300

    Add project files.

---
+++ Dictionary/MyDictonary.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class Dictonary<TValue, TKey>
+    {
+        TValue[] values;
+        TKey[] keys;
+
+        public Dictonary()
+        {
+            values = new TValue[0];
+            keys = new TKey[0];
+        }
+
+        public void Add(TValue val, TKey key)
+        {
+            TValue[] tempVals = values;
+            TKey[] tempKeys = keys;
+
+
+            values = new TValue[values.Length + 1];
+            keys = new TKey[keys.Length + 1];
+
+            for (int i = 0; i < tempVals.Length; i++)
+            {
+                values[i] = tempVals[i];
+                keys[i] = tempKeys[i];
+
+
+            }
+            values[values.Length - 1] = val;
+            keys[keys.Length - 1] = key;
+        }
+        public int Count
+        {
+            get { return values.Length; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictonary<string, int> dictonary = new Dictonary<string, int>();
+
+            dictonary.Add("Ecem", 200);
+            dictonary.Add("Mete", 300);
+            dictonary.Add("Buse", 400);
+            Console.WriteLine(dictonary.Count);
+
+        }
+    }
+}
